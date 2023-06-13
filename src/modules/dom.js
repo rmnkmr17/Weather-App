1a063733b9751bb1de7d7166c93774099e843dd8
rmkkmr17 <ramkum94@tutanota.com>
2023-06-13 20:34:03 +0200

    Added the toggle button

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,6 +2,7 @@ import { getWeatherData } from "./api";
 
 const renderWeather = (data) => {
   const weatherContent = document.querySelector(".weather-content");
+  weatherContent.innerHTML = "";
   const locationName = data.location.name;
   const countryName = data.location.country;
   const humidity = data.current.humidity;
@@ -9,19 +10,46 @@ const renderWeather = (data) => {
   const tempInCelsius = data.current.feelslike_c;
   const wind = data.current.wind_kph;
   const tempInFahreneit = data.current.feelslike_f;
-  console.log(data);
+
+  let tempUnit = "°C";
+  let currentTemp = tempInCelsius;
+
   weatherContent.innerHTML = `
-  <h3 id="">Weather in ${locationName}, ${countryName}</h3>
-  <h1 id="">${tempInCelsius}°C</h1>
+  <h2 id="">Weather in ${locationName}, ${countryName}</h2>
+  <h1 id="temp">${currentTemp}${tempUnit}</h1>
   <div class="middle-content">
   <img src="${data.current.condition.icon}" alt="" />
     <h2>${currentCondition}</h2>
   </div>
   <div class="bottom-content">
     <h3>Humidity: ${humidity}</h3>
-    <h3>Wind: ${wind} Km/h</h3>
+    <h3>Wind: ${wind} km/h</h3>
   </div>
+  <button id="toggle-unit">Switch to °F</button>
+
   `;
+
+  const toggleUnitButton = document.querySelector("#toggle-unit");
+  const tempEl = document.querySelector("#temp");
+  toggleUnitButton.addEventListener("click", () => {
+    if (tempUnit === "°C") {
+      currentTemp = tempInFahreneit;
+      tempUnit = "°F";
+      toggleUnitButton.textContent = `Switch to °C`;
+    } else {
+      currentTemp = tempInCelsius;
+      tempUnit = "°C";
+      toggleUnitButton.textContent = `Switch to °F`;
+    }
+    // Update the temperature display
+    tempEl.textContent = `${currentTemp}${tempUnit}`;
+  });
+  document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/1600x900/?" +
+    currentCondition +
+    " " +
+    locationName +
+    "')";
 };
 
 const loadWeather = () => {
